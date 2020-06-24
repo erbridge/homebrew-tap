8b2cd732c74a6f46ba70a816d7964060c325a367
erbridge <f@erbridge.co.uk>
2020-06-24 15:48:14 +0100

    Update GitHub Branch Renamer to v0.2.4

--- Formula/github-branch-renamer.rb
+++ Formula/github-branch-renamer.rb
@@ -1,8 +1,8 @@
 class GithubBranchRenamer < Formula
   desc "Rename the master branch in many repositories at once"
   homepage "https://github.com/erbridge/github-branch-renamer"
-  url "https://github.com/erbridge/github-branch-renamer/archive/v0.2.3.tar.gz"
-  sha256 "bf1c5741ff61e57df590b118bfa540e5b8642766d6ddabe4a072cc53095c341e"
+  url "https://github.com/erbridge/github-branch-renamer/archive/v0.2.4.tar.gz"
+  sha256 "2082e9ebed8aac6c8c72386e8fc8e4f50dc8c638be9d96b18c5c5550861b67af"
 
   def install
     bin.install "gbr"
