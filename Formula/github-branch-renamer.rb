2d1d62ec4d46f73fb9d47c1e0781cde30f3aff63
erbridge <f@erbridge.co.uk>
2021-02-03 12:56:13 +0200

    Switch to the core gh dependency

--- Formula/github-branch-renamer.rb
+++ Formula/github-branch-renamer.rb
@@ -4,7 +4,7 @@ class GithubBranchRenamer < Formula
   url "https://github.com/erbridge/github-branch-renamer/archive/v0.3.0.tar.gz"
   sha256 "b3f983e6e227ace509c425bf156b12c181c75e0550000213bb1f2abde06b9f6e"
 
-  depends_on "github/gh/gh"
+  depends_on "gh"
   depends_on "jq"
 
   def install
